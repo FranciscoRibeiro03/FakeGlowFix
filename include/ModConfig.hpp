1b39aed36efedae2da8e2ec4c42307d66629fc90
Francisco Cabral Ribeiro <francisco.ribeiro@ua.pt>
2022-08-29 02:30:57 +0100

    Change config description

--- include/ModConfig.hpp
+++ include/ModConfig.hpp
@@ -2,7 +2,7 @@
 
 DECLARE_CONFIG(ModConfig,
 
-    CONFIG_VALUE(enabled, bool, "Mod Enabled", true);
+    CONFIG_VALUE(enabled, bool, "Wall Frames Enabled", true);
 
     CONFIG_INIT_FUNCTION(
         CONFIG_INIT_VALUE(enabled);
